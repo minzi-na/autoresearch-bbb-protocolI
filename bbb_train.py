99da8b1dd6d7db23cae59624280b56a2e22c88bd
minzi-na <minzikim@snu.ac.kr>
2026-04-09 11:25:38 +0900

    iter56: pos_weight=0.25 (tuning below iter55 0.28 to find optimal underweighting of positives)

--- bbb_train.py
+++ bbb_train.py
@@ -339,8 +339,8 @@ def run_evaluation(dataset, ext_dataset, holdout_dataset, mod_dims):
             lr=BASE_CONFIG['lr'],
             weight_decay=BASE_CONFIG['weight_decay'],
         )
-        # pos_weight: fixed 0.28 (combo1 tuning found slight underweighting of positives helps)
-        pos_weight = torch.tensor([0.28]).to(device)
+        # pos_weight: fixed 0.25 (tuning below iter55's 0.28 to find optimal underweighting)
+        pos_weight = torch.tensor([0.25]).to(device)
         loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
 
         model = train_model(model, optimizer, train_loader, val_loader, loss_fn)
