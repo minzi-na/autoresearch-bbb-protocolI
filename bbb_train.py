9d51e2ce167e42fbfd88d341d16c3d1ab8b0d9de
minzi-na <minzikim@snu.ac.kr>
2026-04-19 05:46:35 +0900

    iter149: 1.5x LR for fast params (lr=1.5e-4; sweep between 1x and 2x)

--- bbb_train.py
+++ bbb_train.py
@@ -439,7 +439,7 @@ def run_evaluation(dataset, ext_dataset, holdout_dataset, mod_dims):
                 base_params.append(p)
         optimizer = optim.Adam(
             [{'params': base_params, 'lr': lr},
-             {'params': fast_params, 'lr': lr * 2}],
+             {'params': fast_params, 'lr': lr * 1.5}],
             weight_decay=wd,
             amsgrad=True,
         )
